571b3a590ca10af797b2ad5ce29f43b637e6d4d4
Raphael Kubo da Costa <kubito@gmail.com>
2011-04-11 01:25:02 -0300

    Rework FindDBus.cmake.
    Inspired by kde-workspace's FindDBus.cmake, but much shorter.
    
    pkg-config should never be required. Plus the way it used pkgconfig's
    variables was broken: it returns only the library names, not their
    paths, so linking fails when the library is not on a path searched by
    default by the linker.

--- cmake/FindDBus.cmake
+++ cmake/FindDBus.cmake
@@ -1,36 +1,70 @@
-# - Try to find the dbus libraries
+# - Try to find the low-level D-Bus library
 # Once done this will define
 #
-# DBUS_FOUND            - system supports dbus
-# DBUS_INCLUDE_DIRS     - the dbus include directories
-# DBUS_LIBRARY_DIRS     - the directories where the dbus libraries are found
-# DBUS_LIBRARIES        - libdbus or libdbus-1 library
-# DBUSLAUNCH_EXECUTABLE - the path to the dbus-launch executable for testing
+#  DBUS_FOUND - system has D-Bus
+#  DBUS_INCLUDE_DIRS - the D-Bus include directory
+#  DBUS_LIBRARIES - the libraries needed to use D-Bus
+#  DBUS_DBUSLAUNCH_EXECUTABLE - the path to the dbus-launch executable for testing
 
-if(WIN32)
-  file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _program_FILES_DIR)
-  find_path(DBUS_INCLUDE_DIRS dbus/dbus.h
-    ${_program_FILES_DIR}/dbus/include
-    ${CMAKE_INSTALL_PREFIX}/include
-    ${CMAKE_INCLUDE_PATH}
-  )
-  set(DBUS_LIBRARY_DIRS ${_program_FILES_DIR}/dbus/lib)
-  find_library(DBUS_LIBRARIES NAMES dbus dbus-1 dbus-1d
-    PATHS
-      ${DBUS_LIBRARY_DIRS}
-      ${CMAKE_INSTALL_PREFIX}/lib
-      ${CMAKE_LIBRARY_PATH}
-  )
+# Copyright (c) 2011, Raphael Kubo da Costa <kubito@gmail.com>
+# modeled after FindDBus.cmake in kde-workspace:
+# Copyright (c) 2008, Kevin Kofler, <kevin.kofler@chello.at>
+# which was modeled after FindLibArt.cmake:
+# Copyright (c) 2006, Alexander Neundorf, <neundorf@kde.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-if( DBUS_LIBRARIES AND DBUS_INCLUDE_DIRS )
-  set( DBUS_FOUND 1 )
-endif( DBUS_LIBRARIES AND DBUS_INCLUDE_DIRS )
+find_package(PkgConfig)
+pkg_check_modules(PC_DBUS dbus-1)
 
-else(WIN32)
+find_path(DBUS_INCLUDE_DIR NAMES dbus/dbus.h
+  HINTS ${PC_DBUS_INCLUDEDIR}
+        ${PC_DBUS_INCLUDE_DIRS}
+  PATH_SUFFIXES dbus-1.0
+)
 
-  find_package(PkgConfig REQUIRED)
-  pkg_check_modules(DBUS dbus-1>=1.0)
+find_path(DBUS_ARCH_INCLUDE_DIR dbus/dbus-arch-deps.h
+  HINTS ${PC_DBUS_INCLUDEDIR}
+        ${PC_DBUS_INCLUDE_DIRS}
+  PATHS /usr/lib${LIB_SUFFIX}/include
+        /usr/lib${LIB_SUFFIX}/dbus-1.0/include
+        /usr/lib64/include
+        /usr/lib64/dbus-1.0/include
+        /usr/lib/include
+        /usr/lib/dbus-1.0/include
+)
 
-endif(WIN32)
+set(DBUS_INCLUDE_DIRS ${DBUS_INCLUDE_DIR} ${DBUS_ARCH_INCLUDE_DIR})
 
-find_program(DBUSLAUNCH_EXECUTABLE NAMES dbus-launch)
+find_library(DBUS_LIBRARIES NAMES dbus-1 dbus
+  HINTS ${PC_DBUS_LIBDIR}
+        ${PC_DBUS_LIBRARY_DIRS}
+)
+
+find_program(DBUS_DBUSLAUNCH_EXECUTABLE NAMES dbus-launch)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(DBUS DEFAULT_MSG DBUS_LIBRARIES DBUS_INCLUDE_DIRS DBUS_DBUSLAUNCH_EXECUTABLE)
+
+mark_as_advanced(DBUS_LIBRARIES DBUS_INCLUDE_DIRS DBUS_DBUSLAUNCH_EXECUTABLE)
